a4eec284da4dfdd31a92785d08520402fc34ea68
Stephan Meissl <stephan@meissl.name>
2015-01-24 13:30:56 -0800

    Initial scaffolding.

---
+++ app/scripts/main.js
@@ -0,0 +1,26 @@
+var terrain = new L.tileLayer('//{s}.tiles.maps.eox.at/wmts/1.0.0/terrain-light_3857/default/g/{z}/{y}/{x}.jpg', {
+    minZoom: 0,
+    maxZoom: 18,
+    attribution: 'Map data © <a href="http://www.openstreetmap.org">OpenStreetMap contributors</a>'
+});
+var overlay = new L.tileLayer('//{s}.tiles.maps.eox.at/wmts/1.0.0/overlay_3857/default/g/{z}/{y}/{x}.jpg', {
+    minZoom: 0,
+    maxZoom: 18,
+    attribution: 'Map data © <a href="http://www.openstreetmap.org">OpenStreetMap contributors</a>'
+});
+
+var map = L.map('map', {
+    layers: [terrain]
+});
+
+var baseMaps = {
+    "Terrain": terrain
+};
+var overlayMaps = {
+    "Overlay": overlay
+};
+
+L.control.layers(baseMaps, overlayMaps).addTo(map);
+L.control.scale().addTo(map);
+
+map.fitWorld()
